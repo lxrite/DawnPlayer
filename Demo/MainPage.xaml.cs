6b063a58a0ee14336c6be855d33091a0cfa10454
limhiaoing <lxrite@gmail.com>
2015-04-02 23:43:53 +0800

    Update Demo Hide 'Stop' button Add seekBar volumeBar

--- Demo/MainPage.xaml.cs
+++ Demo/MainPage.xaml.cs
@@ -17,6 +17,7 @@ using Microsoft.Phone.Shell;
 using System.Windows.Media;
 using System.Diagnostics;
 using Windows.Storage.Streams;
+using System.Windows.Threading;
 
 using DawnPlayer;
 
@@ -24,10 +25,28 @@ namespace Demo
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private bool isUserSeek = false;
         private IRandomAccessStream randomAccessStream;
         public MainPage()
         {
             InitializeComponent();
+            var dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += OnTimerEvent;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void OnTimerEvent(object sender, EventArgs e)
+        {
+            var position = mediaElement.Position;
+            var duration = mediaElement.NaturalDuration.TimeSpan;
+            timeText.Text = String.Format("{0}:{1:D2}:{2:D2}/{3}:{4:D2}:{5:D2}",
+                position.Hours, position.Minutes, position.Seconds,
+                duration.Hours, duration.Minutes, duration.Seconds);
+            if (!isUserSeek)
+            {
+                seekBar.Value = position.TotalSeconds;
+            }
         }
 
         private async void OnPlayPauseButtonClick(object sender, RoutedEventArgs e)
@@ -35,18 +54,11 @@ namespace Demo
             var curState = mediaElement.CurrentState;
             if (curState == MediaElementState.Closed)
             {
-                try
-                {
-                    var applicationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
-                    var storageFile = await applicationFolder.GetFileAsync("Assets\\test.flv");
-                    randomAccessStream = await storageFile.OpenReadAsync();
-                    mediaElement.SetSource(FlvMediaStreamSource.Wrap(randomAccessStream));
-                    mediaElement.Play();
-                }
-                catch (Exception except)
-                {
-                    Debug.WriteLine("Exception: " + except.Message);
-                }
+                var applicationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+                var storageFile = await applicationFolder.GetFileAsync("Assets\\test.flv");
+                randomAccessStream = await storageFile.OpenReadAsync();
+                mediaElement.SetSource(FlvMediaStreamSource.Wrap(randomAccessStream));
+                mediaElement.Play();
             }
             else if (curState == MediaElementState.Playing)
             {
@@ -71,11 +83,14 @@ namespace Demo
         private void OnMediaOpened(object sender, RoutedEventArgs e)
         {
             Debug.WriteLine("OnMediaOpened");
+            seekBar.Minimum = 0;
+            seekBar.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
         }
 
         private void OnMediaEnded(object sender, RoutedEventArgs e)
         {
             Debug.WriteLine("OnMediaEnded");
+            mediaElement.Source = null;
         }
 
         private void OnMediaFailed(object sender, RoutedEventArgs e)
@@ -103,6 +118,26 @@ namespace Demo
                     randomAccessStream.Dispose();
                     randomAccessStream = null;
                 }
+            }
+        }
+
+        private void seekBarManipulationStarted(object sender, System.Windows.Input.ManipulationStartedEventArgs e)
+        {
+            isUserSeek = true;
+        }
+
+        private void seekBarManipulationCompleted(object sender, System.Windows.Input.ManipulationCompletedEventArgs e)
+        {
+            mediaElement.Position = TimeSpan.FromSeconds(seekBar.Value);
+            isUserSeek = false;
+        }
+
+        private void volumeBarValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            const double c = 9.21045;
+            if (mediaElement != null)
+            {
+                mediaElement.Volume = Math.Log(e.NewValue * (Math.Pow(Math.E, c) - 1) + 1) / c;
             }
         }
     }
