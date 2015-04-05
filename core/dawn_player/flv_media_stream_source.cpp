82cb081497cb55e54835ce94ff8abe4dd94165c9
limhiaoing <lxrite@gmail.com>
2015-04-05 19:38:33 +0800

    Add dawn_player winrt solution.

---
+++ core/dawn_player/flv_media_stream_source.cpp
@@ -0,0 +1,20 @@
+﻿/*
+*    flv_media_stream_source.cpp:
+*
+*    Copyright (C) 2015 limhiaoing <blog.poxiao.me> All Rights Reserved.
+*
+*/
+
+#include "pch.h"
+
+#include "flv_media_stream_source.hpp"
+
+using namespace Platform;
+
+namespace dawn_player {
+
+flv_media_stream_source::flv_media_stream_source()
+{
+}
+
+} // namespace dawn_player
---
+++ core/dawn_player/flv_media_stream_source.hpp
@@ -0,0 +1,21 @@
+﻿/*
+ *    flv_media_stream_source.hpp:
+ *
+ *    Copyright (C) 2015 limhiaoing <blog.poxiao.me> All Rights Reserved.
+ *
+ */
+
+#ifndef DAWN_PLAYER_FLV_MEDIA_STREAM_SOURCE_HPP
+#define DAWN_PLAYER_FLV_MEDIA_STREAM_SOURCE_HPP
+
+namespace dawn_player
+{
+
+public ref class flv_media_stream_source sealed {
+public:
+    flv_media_stream_source();
+};
+
+} // namespace dawn_player
+
+#endif
